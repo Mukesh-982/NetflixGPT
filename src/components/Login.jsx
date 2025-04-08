ecb937bb3e44c62d4ebfc45ec8e819ab86279152
Mukesh <kmukeshbabu982@gmail.com>
2025-04-08 19:14:41 +0530

    Changed login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,17 @@
 import React, {useState, useRef} from 'react'
-import { background, LOGO_URL, userIcon } from '../constants/constants'
+import { background, userIcon } from '../constants/constants'
 import { checkValidData } from '../utils/validate';
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../utils/firebase';
-import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../redux/userSlice';
+import Header from './Header';
 
 const Login = () => {
     const [isSignInForm, setIsSignInForm] = useState(true);
     const [errorMessage, setErrorMessage] = useState();
-    const navigate = useNavigate();
+    
     const dispatch = useDispatch();
     
     const name = useRef(null);
@@ -41,7 +41,6 @@ const Login = () => {
                         // Redux will update via onAuthStateChanged
                         const {uid, email, displayName, photoURL} = user;
                         dispatch(addUser({uid: uid, email: email, name: displayName, photoURL: photoURL}));
-                        navigate("/browse");
                     })
                     .catch((error) => {
                         setErrorMessage(error.message);
@@ -57,8 +56,6 @@ const Login = () => {
             .then((userCredential)=>{
                 //Signed In
                 const user = userCredential.user;
-                console.log(user);
-                navigate('/browse');
             })
             .catch((error)=>{
                 const errorCode = error.code;
@@ -74,64 +71,63 @@ const Login = () => {
     };
 
     return (
-        <div className="relative w-screen h-screen overflow-hidden flex justify-center items-center"
+        <div className="relative w-screen h-screen overflow-hidden "
             style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
         >
-            <div className='absolute top-0 left-0 right-0 flex justify-between items-center p-4 bg-gradient-to-b from-black'>
-                <div className='w-28 m-4'>
-                    <img src={LOGO_URL} alt="logo" className='w-full h-auto'/>
-                </div>
-            </div>
+            
+            <Header/>
 
-            <div className="bg-black/75 p-8 rounded-lg shadow-lg text-white w-96">
-                <h2 className="text-2xl font-bold mb-4 text-center">{isSignInForm ? "Sign In" : "Sign Up"}</h2>
-                <form onSubmit={(e)=> e.preventDefault()} className="flex flex-col">
-                    {
-                        !isSignInForm && (
-                            <input type="text" 
-                                ref={name}
-                                placeholder='Full Name'
-                                className='p-3 mb-4 rounded-md bg-gray-800 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-red-500'
-                            />
-                        )
-                    }
-                    <input 
-                        type="text"
-                        ref={email} 
-                        placeholder="Email address" 
-                        className="p-3 mb-4 rounded-md bg-gray-800 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-red-500"
-                    />
-                    <input 
-                        type="password" 
-                        ref={password}
-                        placeholder="Password" 
-                        className="p-3 mb-4 rounded-md bg-gray-800 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-red-500"
-                    />
-                    {errorMessage && (
-                        <p className='p-3 mb-4 text-lg font-bold text-red-600 text-center'>{errorMessage}</p>
-                    )}
-                    <button 
-                        className="bg-red-600 hover:bg-red-700 transition-all p-3 rounded-md font-semibold text-white cursor-pointer"
-                        onClick={handleButtonClick}
-                    >
-                        {isSignInForm ? "Sign In" : "Sign Up"}
-                    </button>
-                </form>
-                <p onClick={toggleSignInForm} className="text-sm text-gray-400 mt-4 text-center">
-                    {
-                        isSignInForm ? (
-                            <>
-                                New to Our Service? 
-                                <span className="text-red-500 cursor-pointer hover:underline ml-1">Sign up now</span>
-                            </>
-                        ) : (
-                            <>
-                                Already existing user?
-                                <span className="text-red-500 cursor-pointer hover:underline ml-1">Sign in now</span>
-                            </>
-                        )
-                    }
-                </p>
+            <div className='flex justify-center items-center h-[calc(100vh-80px)] mt-4'>
+                <div className="bg-black/75 p-8 rounded-lg shadow-lg text-white w-96">
+                    <h2 className="text-2xl font-bold mb-4 text-center">{isSignInForm ? "Sign In" : "Sign Up"}</h2>
+                    <form onSubmit={(e)=> e.preventDefault()} className="flex flex-col">
+                        {
+                            !isSignInForm && (
+                                <input type="text" 
+                                    ref={name}
+                                    placeholder='Full Name'
+                                    className='p-3 mb-4 rounded-md bg-gray-800 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-red-500'
+                                />
+                            )
+                        }
+                        <input 
+                            type="text"
+                            ref={email} 
+                            placeholder="Email address" 
+                            className="p-3 mb-4 rounded-md bg-gray-800 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-red-500"
+                        />
+                        <input 
+                            type="password" 
+                            ref={password}
+                            placeholder="Password" 
+                            className="p-3 mb-4 rounded-md bg-gray-800 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-red-500"
+                        />
+                        {errorMessage && (
+                            <p className='p-3 mb-4 text-lg font-bold text-red-600 text-center'>{errorMessage}</p>
+                        )}
+                        <button 
+                            className="bg-red-600 hover:bg-red-700 transition-all p-3 rounded-md font-semibold text-white cursor-pointer"
+                            onClick={handleButtonClick}
+                        >
+                            {isSignInForm ? "Sign In" : "Sign Up"}
+                        </button>
+                    </form>
+                    <p onClick={toggleSignInForm} className="text-sm text-gray-400 mt-4 text-center">
+                        {
+                            isSignInForm ? (
+                                <>
+                                    New to Our Service? 
+                                    <span className="text-red-500 cursor-pointer hover:underline ml-1">Sign up now</span>
+                                </>
+                            ) : (
+                                <>
+                                    Already existing user?
+                                    <span className="text-red-500 cursor-pointer hover:underline ml-1">Sign in now</span>
+                                </>
+                            )
+                        }
+                    </p>
+                </div>
             </div>
             
                 
